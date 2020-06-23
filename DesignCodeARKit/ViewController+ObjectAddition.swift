bd1db0565720a5519f43d7dfebd7ab5410ff1cba
fzaw <fzaw@matterport.com>
2020-06-22 18:45:29 -0700

    object can be positioned

--- DesignCodeARKit/ViewController+ObjectAddition.swift
+++ DesignCodeARKit/ViewController+ObjectAddition.swift
@@ -24,6 +24,18 @@ extension ViewController {
         print("Add button tapped")
         
         guard focusSquare != nil else {return}
+        let modelName = "iPhoneX"
+        guard let model = getModel(named: modelName) else {
+            print("Unable to load \(modelName) from file")
+            return
+        }
+        
+        let hitTest = sceneView.hitTest(screenCenter, types: .existingPlaneUsingExtent)
+        guard let worldTransformColumn3 = hitTest.first?.worldTransform.columns.3 else {return}
+        model.position = SCNVector3(worldTransformColumn3.x, worldTransformColumn3.y, worldTransformColumn3.z)
+        
+        sceneView.scene.rootNode.addChildNode(model)
+        print("\(modelName) added successfully")
     }
     
 }
